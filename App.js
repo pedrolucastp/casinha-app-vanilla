c07e9efbffa7d9e4406fd331006b04d1e08300e0
pedrolucastp <pedrolucasp@gmail.com>
2021-12-08 04:33:53 -0300

    fix: App.js

--- App.js
+++ App.js
@@ -1,4 +1,20 @@
-import bills from "./Bills.js"
+import bills from "./Bills.js";
 
 console.log("Casinha App");
 console.log(bills);
+
+// criar uma função que recebe uma lista (array) contendo
+//  a serem convertidos para elementos (objetos)
+// e tranforme os elemtneos em algo visual
+
+function newCard(list) {
+  const cardTemplate = document.querySelector("[data-card-template]");
+  const cardContainer = cardTemplate.content.cloneNode(true);
+  const cardContainer = document.querySelector("[data-card-info]");
+
+  list.forEach((card) => {
+    console.log()
+  });
+}
+
+newCard(bills);
