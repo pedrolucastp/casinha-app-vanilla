7cdb9bbd3dfdb5e06fa9d47b36a7c3c36ea51952
pedrolucastp <pedrolucasp@gmail.com>
2021-12-08 03:16:10 -0300

    fix: Bills.js bills

--- Bills.js
+++ Bills.js
@@ -1,9 +1,27 @@
 const bills = [
-    {
-        billName: "Poda",
-        billPrice: "R$ 100,00"
-    }
-]
+  {
+    billName: "Poda",
+    billPrice: "R$ 100,00",
+  },
 
+  {
+    billName: "Aluguel",
+    billPrice: "R$ 1000,00",
+  },
 
+  {
+    billName: "Luz",
+    billPrice: "R$ 160,00",
+  },
+
+  {
+    billName: "Água",
+    billPrice: "R$ 150,00",
+  },
+
+  {
+    billName: "Internet",
+    billPrice: "R$ 89,00",
+  },
+];
 export default bills;
