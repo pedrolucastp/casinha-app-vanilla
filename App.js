84c4273a30738b9ed08fe0fc600c421e54c4e6d1
pedrolucastp <pedrolucasp@gmail.com>
2021-12-08 16:46:18 -0300

    fix: setMenu

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
 import bills from "./Bills.js";
-import shoppinglist from "./ShoppingList.js"
+import shoppinglist from "./ShoppingList.js";
 console.log("Casinha App");
-
 
 console.log("bills", bills);
 console.log("shoppinglist", shoppinglist);
 
-// criar uma função que recebe uma lista (array) 
+// criar uma função que recebe uma lista (array)
 //  a serem convertidos para elementos (objetos)
 // e tranforme os elemtneos em algo visual
 
-const list = shoppinglist
+function setButtons() {
 
-list.forEach((card) => {
-    const cardBoard = document.querySelector("[data-card-board]");
-    const cardInfo = document.createElement("BUTTON");
-    cardInfo.setAttribute("data-card-layout", "");
-    const cardTitle = document.createElement("H2");
-    cardTitle.innerText = card.title;
-    cardInfo.appendChild(cardTitle);
-    const cardPrice = document.createElement("H3");
-    cardPrice.innerText = card.price;
-    cardInfo.appendChild(cardPrice);
-    const cardDescription = document.createElement("P");
-    cardDescription.innerText = card.description;
-    cardInfo.appendChild(cardDescription);
-    cardBoard.appendChild(cardInfo);
-});
+    const button = document.querySelector("[data-menu-button-1]");
+    button.innerText = "Bills"
+    button.addEventListener(
+        "click", setCard(bills));
+}
+
+setButtons()
+
+function setCard(list) {
+    list.forEach((card) => {
+        const cardBoard = document.querySelector("[data-card-board]");
+        const cardInfo = document.createElement("BUTTON");
+        cardInfo.setAttribute("data-card-layout", "");
+        const cardTitle = document.createElement("H2");
+        cardTitle.innerText = card.title;
+        cardInfo.appendChild(cardTitle);
+        const cardPrice = document.createElement("H3");
+        cardPrice.innerText = card.price;
+        cardInfo.appendChild(cardPrice);
+        const cardDescription = document.createElement("P");
+        cardDescription.innerText = card.description;
+        cardInfo.appendChild(cardDescription);
+        cardBoard.appendChild(cardInfo);
+        return;
+    });
+}
+
+setCard(bills);
