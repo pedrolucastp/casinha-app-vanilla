1fc1c9fd9965aa4e1aa980b597a6b31d5dd71b5f
pedrolucastp <pedrolucasp@gmail.com>
2021-12-08 08:23:41 -0300

    fix: layout

--- Bills.js
+++ Bills.js
@@ -1,32 +1,46 @@
-const bills = [
-  {
-    title: "Aluguel",
-    price: "R$ 1000,00",
-    description: "lorem5",
-  },
+const bills = [{
+        title: "Aluguel",
+        price: "R$ 1000,00",
+        description: "lorem5",
+    },
 
-  {
-    title: "Luz",
-    price: "R$ 160,00",
-    description: "lorem5",
-  },
+    {
+        title: "Luz",
+        price: "R$ 160,00",
+        description: "lorem5",
+    },
 
-  {
-    title: "Água",
-    price: "R$ 150,00",
-    description: "lorem5",
-  },
+    {
+        title: "Água",
+        price: "R$ 150,00",
+        description: "lorem5",
+    },
 
-  {
-    title: "Internet",
-    price: "R$ 89,00",
-    description: "lorem5",
-  },
-  {
-    title: "Poda",
-    price: "R$ 100,00",
-    description: "lorem5",
-  },
+    {
+        title: "Internet",
+        price: "R$ 89,00",
+        description: "lorem5",
+    },
+    {
+        title: "Poda",
+        price: "R$ 100,00",
+        description: "lorem5",
+    },
+    {
+        title: "Poda",
+        price: "R$ 100,00",
+        description: "lorem5",
+    },
+    {
+        title: "Poda",
+        price: "R$ 100,00",
+        description: "lorem5",
+    },
+    {
+        title: "Poda",
+        price: "R$ 100,00",
+        description: "lorem5",
+    },
 ];
 
 export default bills;
