7596837e7212f7678e452e6b9576c75ed7907590
pedrolucastp <pedrolucasp@gmail.com>
2021-12-09 03:15:20 -0300

    add: color picker

--- App.js
+++ App.js
@@ -2,6 +2,20 @@ import bills from "./Bills.js";
 import shoppingList from "./ShoppingList.js";
 console.log("Casinha App");
 
+//trocar o backgroud a cada click no header
+const themeChanger = document.getElementById("themeChanger");
+console.log(themeChanger);
+themeChanger.onclick = function(event) {
+    if (event) {
+        document.body.style.setProperty("background-color", "#fc5296");
+        document.body.style.setProperty("background-image", "linear-gradient(315deg, #fc5296 0%, #f67062 74%)");
+        return;
+    }
+
+    document.body.style.setProperty("background-color", "#20bf55");
+    document.body.style.setProperty("background-image", "linear-gradient(326deg, #e020a7 0%, #5f0a87 74%)");
+};
+
 const sections = [
     { name: "Contas", value: bills },
     { name: "Compras", value: shoppingList },
