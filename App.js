3bbc9e1bd17611d92310fdcd5a1f468549e82a29
pedrolucastp <pedrolucasp@gmail.com>
2021-12-12 19:42:46 -0300

    add: list id

--- App.js
+++ App.js
@@ -2,49 +2,23 @@ import favorites from "./favorites.js";
 console.log("Casinha App");
 
 //trocar o backgroud a cada click no header
-
-document.querySelector("[data-theme-changer]").onclick = function(e) {
-    if (e) {
-        document.body.style.backgroundColor = "#0cbaba";
-        document.body.style.backgroundImage =
-            "linear-gradient(315de g, #0cbaba 0%, #380036 74%)";
-        return;
-    }
-};
+{
+    document.querySelector("[data-theme-changer]").onclick = function(e) {
+        if (e) {
+            document.body.style.backgroundColor = "#0cbaba";
+            document.body.style.backgroundImage =
+                "linear-gradient(315de g, #0cbaba 0%, #380036 74%)";
+            return;
+        }
+    };
+}
 
 let newListFavorites = favorites;
-console.log(newListFavorites)
+console.log("newListFavorites", newListFavorites)
 
-// adicionar novo lista
-document.querySelector("[data-new-favorite]").onclick = () => {
-    console.log("new list");
-    const newFavorite = {
-        name: prompt("name new favorite"),
-        value: [],
-    };
-    newListFavorites.push(newFavorite);
-    setButton(newListFavorites);
-    return;
-};
-// remover a lista atual
+// Criar os botoes de favoritos  
 
-document.querySelector("[data-remove-favorite]").onclick = () => {
-    console.log("remove list");
-    const removeFavorite = prompt("name remove favorite");
-
-    const filteredFavorites = newListFavorites.filter(function(el) {
-        return el.name !== removeFavorite;
-    });
-    newListFavorites = filteredFavorites;
-    setButton(newListFavorites);
-};
-
-
-// Criar os botoes de favoritos dinamicamente com os diferentes segmentos de um array
-// Conectar os botoes ao DOM dinamicamente
-
-
-function setButton(lists) {
+function setFavorites(lists) {
     const favoriteContainer = document.querySelector("[data-favorite-container]");
     favoriteContainer.innerText = "";
     lists.forEach((list) => {
@@ -52,25 +26,50 @@ function setButton(lists) {
         button.innerText = list.name;
         button.setAttribute("id", list.name);
         button.onclick = () => {
-            return setCard(list.value);
+            console.log(list)
+            setCard(list);
         };
 
         favoriteContainer.appendChild(button);
     });
 }
 
-setButton(newListFavorites);
+setFavorites(newListFavorites);
 
-//  a serem convertidos para elementos (objetos)
+// adicionar novo lista
+
+document.querySelector("[data-new-favorite]").onclick = () => {
+    console.log("new list");
+    const newFavorite = {
+        name: prompt("name new favorite"),
+        value: [],
+    };
+    newListFavorites.push(newFavorite);
+    setFavorites(newListFavorites);
+    return;
+};
+
+// remover a lista atual
+document.querySelector("[data-remove-favorite]").onclick = () => {
+    console.log("remove list");
+    const removeFavorite = prompt("name remove favorite");
+    const filteredFavorites = newListFavorites.filter(function(el) {
+        return el.name !== removeFavorite;
+    });
+    newListFavorites = filteredFavorites;
+    setFavorites(newListFavorites);
+};
+
+
 // criar uma função que recebe uma lista (array)
 // e tranforme os elemtneos em algo visual (cards)
 
 function setCard(list) {
     const cardBoard = document.querySelector("[data-card-board]");
     cardBoard.innerText = "";
-    cardBoard.setAttribute("data-current-list", list.name)
-
-    list.forEach((card) => {
+    console.log(list.name)
+    cardBoard.setAttribute("data-card-board", "")
+    list.value.forEach((card) => {
         const cardInfo = document.createElement("BUTTON");
 
         const cardTitle = document.createElement("H2");
@@ -90,27 +89,22 @@ function setCard(list) {
         return;
     });
 }
-//  adicionar novo item a lista atual
 
-function setNewItem(list) {
-    document.querySelector("[data-card-board]")
-
-    // name: prompt("name new favorite"),
-    // value: [],
-    document.querySelector("[data-new-item]").onclick = () => {
-        console.log("new item");
-
-        if (cardBoard.innerText !== "") {
-
-            const newItem = {
-                title: prompt("TÍTULO"),
-                price: prompt("PREÇO"),
-                description: prompt("DESCRIÇÃO"),
-            };
-            currentList.innerText = "Selecione uma lista"
-        }
-        currentList.push(newItem);
-        setCard(newListFavorites);
-        return;
+// funçao criar novo item
+function handleNewItem() {
+    const currentList = document.querySelector("[data-card-doard]")
+    const newItem = {
+        title: prompt("TÍTULO"),
+        price: prompt("PREÇO"),
+        description: prompt("DESCRIÇÃO"),
     };
 }
+
+//  adicionar novo item a lista atual
+
+document.querySelector("[data-new-item]").onclick = () => {
+    console.log("new item");
+    console.log(newItem)
+
+    return;
+};
